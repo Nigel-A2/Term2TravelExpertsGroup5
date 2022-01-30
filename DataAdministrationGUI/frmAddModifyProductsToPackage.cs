9ee4be2dc4ff50c038f16647da8a3adff4230be7
NigelOOSD <nigel.awrey@edu.sait.ca>
2022-01-30 11:11:49 -0600

    I'm using Filip's design for the bridge table forms because it has the least lines of code.

--- DataAdministrationGUI/frmAddModifyProductsToPackage.cs
+++ DataAdministrationGUI/frmAddModifyProductsToPackage.cs
@@ -12,9 +12,69 @@ namespace DataAdministrationGUI
 {
 	public partial class frmAddModifyProductsToPackage : Form
 	{
+
+		/*
+		* public bool isAdd;
+		public Product product;
+		*/
+
 		public frmAddModifyProductsToPackage()
 		{
 			InitializeComponent();
 		}
+
+		private void frmAddModifyProductsToPackage_Load(object sender, EventArgs e)
+		{
+			/*
+			 * if (isAdd)
+            {
+                this.Text = "Add Product";
+                tbxProductId.Text = "ID is generated automatically";
+            }
+            else
+            {
+                this.Text = "Modify Product";
+                if (product == null)
+                {
+                    MessageBox.Show("There is no data of selected product", "Modify Error");
+                    this.Close();
+                }
+                else
+                {
+                    tbxProductId.Text = Convert.ToString(product.ProductId);
+                    tbxProductName.Text = product.ProdName;
+                }
+            }
+			 */
+		}
+
+		private void btnConfirm_Click(object sender, EventArgs e)
+		{
+			/*
+			 * if (ValidateProduct())
+            {
+                if (isAdd)
+                {
+                    product = new Product();
+                }
+                product.ProdName = tbxProductName.Text;
+                this.DialogResult = DialogResult.OK;
+            }
+			 */
+		}
+
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			/*
+			 * this.Close();
+			 */
+		}
+
+		/*
+		 * private bool ValidateProduct()
+        {
+            return Validator.IsPresent(tbxProductName) && tbxProductName.TextLength <= Product.MAX_NAME_LENGTH;
+        }
+		 */
 	}
 }
