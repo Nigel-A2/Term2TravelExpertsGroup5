a0e6f02f92bdc4c50def2f8e6d47452c846aa155
Filip <fflorek.7@gmail.com>
2022-02-02 14:16:55 -0700

    Display and delete products in packages

---
+++ DataAdministrationGUI/Models/PackageDisplayObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAdministrationGUI.Models
+{
+    public class PackageDisplayObject
+    {
+        public int PackageId { get; set; }
+        public int ProductSupplierId { get; set; }
+        public string PackageName { get; set; }
+        public string ProductName { get; set; }
+        public string SupplierName { get; set; }
+
+        public string GetDisplayText(string sep)
+        {
+            return PackageName.PadRight(30) + sep + ProductName.PadRight(30) + sep + SupplierName.PadRight(30);
+        }
+
+    }
+}
---
+++ DataAdministrationGUI/frmProductsInPackageMaintenance.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataAdministrationGUI.Models;
+
+namespace DataAdministrationGUI
+{
+	public partial class frmProductsInPackageMaintenance : Form
+	{
+		private List<PackageDisplayObject> productsInPackages;
+
+		public frmProductsInPackageMaintenance()
+		{
+			InitializeComponent();
+		}
+
+		private void frmProductsInsidePackage_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				using (TravelExpertsContext db = new TravelExpertsContext())
+				{
+
+					var packageQuery =
+						from p in db.Products
+						join ps in db.ProductsSuppliers on p.ProductId equals ps.ProductId
+						join s in db.Suppliers on ps.SupplierId equals s.SupplierId
+						join pps in db.PackagesProductsSuppliers on ps.ProductSupplierId equals pps.ProductSupplierId
+						join pckg in db.Packages on pps.PackageId equals pckg.PackageId
+						orderby pckg.PkgName, p.ProdName descending
+						select new PackageDisplayObject()
+						{
+							PackageId = pckg.PackageId,
+							ProductSupplierId = ps.ProductSupplierId,
+							PackageName = pckg.PkgName,
+							ProductName = p.ProdName,
+							SupplierName = s.SupName
+						};
+
+					productsInPackages = packageQuery.ToList();
+						
+					FillProductsInPackagesListBox();
+				}
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show("Error while getting packages-products-suppliers data: " + exception.Message, exception.GetType().ToString());
+			}
+		}
+
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+
+		}
+
+
+
+		private void btnModify_Click(object sender, EventArgs e)
+		{
+
+		}
+
+
+
+		private void btnDelete_Click(object sender, EventArgs e)
+		{
+			int selectedIndex = lbxPackageProducts.SelectedIndex;
+			if (selectedIndex != -1)
+			{
+				PackageDisplayObject selectedProductInPackage = productsInPackages[selectedIndex];
+				DialogResult answer = MessageBox.Show($"Are you sure you want to delete {selectedProductInPackage.ProductName} from package: {selectedProductInPackage.PackageName}?",
+					"Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+				if (answer == DialogResult.Yes)
+				{
+					DeleteProduct(new PackagesProductsSupplier 
+					{	
+						ProductSupplierId = selectedProductInPackage.ProductSupplierId,
+						PackageId = selectedProductInPackage.PackageId
+					});
+				}
+			}
+			else
+			{
+				MessageBox.Show("You need to select a package-product-supplier!", "Delete Aborted");
+			}
+		}
+
+
+		private void DeleteProduct(PackagesProductsSupplier productInPackage)
+		{
+			try
+			{
+				using (TravelExpertsContext db = new TravelExpertsContext())
+				{
+					db.PackagesProductsSuppliers.Remove(productInPackage);
+					db.SaveChanges();
+				}
+				productsInPackages.Remove
+					(
+						productsInPackages.Find
+						(
+							p => p.PackageId == productInPackage.PackageId &&
+							p.ProductSupplierId == productInPackage.ProductSupplierId
+						)
+					);
+				FillProductsInPackagesListBox();
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show("Error while deleting product-supplier! " + exception.Message, exception.GetType().ToString());
+			}
+		}
+
+		private void FillProductsInPackagesListBox()
+		{
+			lbxPackageProducts.Items.Clear();
+			foreach (PackageDisplayObject p in productsInPackages)
+			{
+				lbxPackageProducts.Items.Add(p.GetDisplayText("\t"));
+			}
+		}
+
+
+	}
+}
